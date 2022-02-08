a958c6a6eabfa8fb95b57ce280303b8c333b8b6a
Théo Boudier <theoleaoboudier@gmail.com>
2022-02-08 11:12:52 +0100

    finish

--- Examen_Cluedo/js/main.js
+++ Examen_Cluedo/js/main.js
@@ -1,35 +1,99 @@
-//Boudier Théo | utiliser mode strict
+//Théo Boudier
 "use strict";
+
 /* 
-    UTILS
+    CONSTANTS
+*/
+const [suspectsSelect, weaponsSelect, roomsSelect, chargeButton, form] = [document.querySelector("#suspects"), document.querySelector("#weapons"), document.querySelector("#rooms"), document.querySelector("#accusation"), document.querySelector('body > form')]
+
+
+/*
+    FUNCTIONS
 */
 
+/** Randomly returns an element of an array
+ * @param { Object } datas
+ * @param { HTMLElement } suspectsSelect
+ * @param { HTMLElement } weaponsSelect
+ * @param { HTMLElement } roomsSelect
+ * @return { Object }
+ */
 function getRandomItemFromArray(array) { return array[Math.floor(Math.random() * array.length)] };
 
-//Retrieve dom elements 
-const [suspectsSelect, weaponsSelect, roomsSelect, chargeButton] = [document.querySelector("#suspects"), document.querySelector("#weapons"), document.querySelector("#rooms"), document.querySelector("#accusation")]
-//Populate selects on window load
+/** Populate selects
+ * @param { Object } datas
+ * @param { HTMLElement } suspectsSelect
+ * @param { HTMLElement } weaponsSelect
+ * @param { HTMLElement } roomsSelect
+ * @return { Object }
+ */
 function populateSelects(datas, suspectsSelect, weaponsSelect, roomsSelect) {
     datas.suspects.map((suspect, index) => suspectsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${suspect}</option>`))
     datas.weapons.map((weapon, index) => weaponsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${weapon}</option>`))
     datas.rooms.map((room, index) => roomsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${room}</option>`))
 }
+
+/** Returns randomly the name of a coupable, a weapon and a room
+ * @param { Object } datas
+ * @return { Object }
+ */
 function getRandomCoupable(datas) {
-    return {
-        name: getRandomItemFromArray(datas.suspects),
-        weapon: getRandomItemFromArray(datas.weapons),
-        room: getRandomItemFromArray(datas.rooms)
-    }
+    return { name: getRandomItemFromArray(datas.suspects), weapon: getRandomItemFromArray(datas.weapons), room: getRandomItemFromArray(datas.rooms) }
 }
-console.log(getRandomCoupable(datas))
 
-//Main
+/** Returns the result of the user inputs
+ * @param { Object } datas
+ * @return { Object }
+ */
+function getFormDatas(datas) {
+    let [nameIndex, weaponIndex, roomIndex] = [document.querySelector('#suspects').value, document.querySelector('#weapons').value, document.querySelector('#rooms').value]
+    return { name: datas.suspects[nameIndex], weapon: datas.weapons[weaponIndex], room: datas.rooms[roomIndex] }
+}
+
+/** Returns the result of the user inputs
+ * @param { Object } coupable
+ * @param { Object } formDatas
+ * @return { Object }
+ */
+function chargeResult(coupable, formDatas) {
+    return { name: coupable.name === formDatas.name, weapon: coupable.weapon === formDatas.weapon, room: coupable.room === formDatas.room }
+}
+
+/*
+    MAIN CODE
+*/
+
+//populate select on window load
 window.addEventListener('onload', populateSelects(datas, suspectsSelect, weaponsSelect, roomsSelect))
-//Gestion du résultat - /6
 
-//Optimisation (bien organisé) - /4 
-
-//Nom des variables - /1
-
-//Comentaires - /3
-
+chargeButton.addEventListener('click', () => {
+    let coupable = getRandomCoupable(datas);
+    let formDatas = getFormDatas(datas);
+    let result = chargeResult(coupable, formDatas);
+    let isWin = result.name && result.weapon && result.room;
+    document.body.insertAdjacentHTML('beforeend', `
+    <table>
+        <thead>
+            <tr>
+                <td>Coupable</td>
+                <td>Arme</td>
+                <td>Pièce</td>
+                </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${formDatas.name}</td>
+                <td>${formDatas.weapon}</td>
+                <td>${formDatas.room}</td>
+            </tr>
+            <tr>
+                <td>${result.name ? '✅' : '❌'}</td>
+                <td>${result.weapon ? '✅' : '❌'}</td>
+                <td>${result.room ? '✅' : '❌'}</td>
+            </tr>
+        </tbody>
+    </table>
+    `);
+    document.body.insertAdjacentHTML('beforeend', `${isWin ? '<b>Bravo !</b>' : '<b>Et c\'est raté !</b>'} C'est <b>${coupable.name}</b> qui a tué le docteur Lenoir avec la/le ${coupable.weapon} dans la/le ${coupable.room}`);
+    form.style.display = 'none';
+})
