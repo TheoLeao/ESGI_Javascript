fad0eb626244d220a7766a9c5ea8b2f3bcc54f87
Théo Boudier <theoleaoboudier@gmail.com>
2022-02-08 10:04:51 +0100

    create getRandomCoupable

--- Examen_Cluedo/js/main.js
+++ Examen_Cluedo/js/main.js
@@ -1,15 +1,29 @@
 //Boudier Théo | utiliser mode strict
 "use strict";
-//Charger les selects au chargement de la page
-const suspectsSelect = document.querySelector("#suspects");
-const weaponsSelect = document.querySelector("#weapons");
-const roomsSelect = document.querySelector("#rooms");
+/* 
+    UTILS
+*/
 
+function getRandomItemFromArray(array) { return array[Math.floor(Math.random() * array.length)] };
+
+//Retrieve dom elements 
+const [suspectsSelect, weaponsSelect, roomsSelect, chargeButton] = [document.querySelector("#suspects"), document.querySelector("#weapons"), document.querySelector("#rooms"), document.querySelector("#accusation")]
+//Populate selects on window load
 function populateSelects(datas, suspectsSelect, weaponsSelect, roomsSelect) {
     datas.suspects.map((suspect, index) => suspectsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${suspect}</option>`))
     datas.weapons.map((weapon, index) => weaponsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${weapon}</option>`))
     datas.rooms.map((room, index) => roomsSelect.insertAdjacentHTML('beforeend', `<option value=${index}>${room}</option>`))
 }
+function getRandomCoupable(datas) {
+    return {
+        name: getRandomItemFromArray(datas.suspects),
+        weapon: getRandomItemFromArray(datas.weapons),
+        room: getRandomItemFromArray(datas.rooms)
+    }
+}
+console.log(getRandomCoupable(datas))
+
+//Main
 window.addEventListener('onload', populateSelects(datas, suspectsSelect, weaponsSelect, roomsSelect))
 //Gestion du résultat - /6
 
